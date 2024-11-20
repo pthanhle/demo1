dcb2b5068761870424074567070affab7f07e481
pthanhle <22110228@student.hcmute.edu.vn>
2024-11-20 15:15:04 +0700

    firstcommit

---
+++ src/test/java/phuocthanh/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package phuocthanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
